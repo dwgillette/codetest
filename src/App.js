680689c15825e7e8a81729b1361700ce1f9d771b
Daniel Gillette <daniel.w.gillette@gmail.com>
2019-07-20 16:30:12 -0700

    basic react setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1>React is working</h1>
+        </header> 
+      </div>
+    );
+  }
+}
+
+export default App;
