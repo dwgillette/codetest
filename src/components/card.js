374afbe039497757ffccaa6014dc1846be25e624
Daniel Gillette <daniel.w.gillette@gmail.com>
2019-07-20 18:41:24 -0700

    card formatting

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Card = props => (
-  <div>
-    <p>{props.card.name}</p>
+  <div className="cardContainer">
+    <div className="imgContainer">
+      <img className="img" src={props.card.picture} alt={props.card.name}></img>
+    </div>
+    {/*<div style={{ 'background-image': 'url(' + props.card.picture + ');'}}></div>*/}
+    <h2>{props.card.name}</h2>
     <p>{props.card.desc}</p>
-    <p>{props.card.fact}</p>
-    <p>{props.card.picture}</p>
+    <p>
+      <strong>Fun fact: </strong>
+      <br></br>
+      {props.card.fact}
+    </p>
   </div>
 )
 
